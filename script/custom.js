90278a46792c9695e23539d53bc4523332d6c80e
AnmolJaiswal-7 <anmoljaiswal380@gmail.com>
2025-06-02 10:40:00 +0530

    fixes

---
+++ script/custom.js
@@ -0,0 +1,19 @@
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      const targetId = this.getAttribute('href').slice(1);
+      const targetElement = document.getElementById(targetId);
+
+      if (targetElement) {
+        e.preventDefault();
+
+        const headerOffset = 80; // change based on your navbar height
+        const elementPosition = targetElement.getBoundingClientRect().top;
+        const offsetPosition = elementPosition + window.scrollY - headerOffset;
+
+        window.scrollTo({
+          top: offsetPosition,
+          behavior: "smooth"
+        });
+      }
+    });
+  });
